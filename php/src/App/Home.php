2e8a99245223f39409c12f488520030359e6f7cc
tromar-8 <53008115+tromar-8@users.noreply.github.com>
2020-09-26 23:13:24 +1000

    Update Home.php

--- php/src/App/Home.php
+++ php/src/App/Home.php
@@ -14,7 +14,7 @@ class Home implements RequestHandlerInterface
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
         return new JsonResponse([
-            'php' => 'PHP is my 2nd least favourite programming language, thankfully the team behind the Laminas project (formally Zend Framework) have created incredibly awesome tools so I don\'t have to worry about the shortcomings of object orientated programming.'
+            'php' => 'PHP is my 2nd least favourite programming language, thankfully the team behind the Laminas project (formerly Zend Framework) have created incredibly awesome tools so I don\'t have to worry about the shortcomings of object orientated programming.'
         ]);
     }
 }
